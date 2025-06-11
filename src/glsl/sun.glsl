c6ebb56f0328c495acf7bd210181a29f7c7c8052
vampireexec <131703298+vampireexec@users.noreply.github.com>
2025-06-11 09:09:31 -0400

    more sun tweaks

--- src/glsl/sun.glsl
+++ src/glsl/sun.glsl
@@ -1,18 +1,21 @@
 //!VAR int usr_var 0
 
-float scale = 300.0;
+float scale = 10.0;
 float usr_p = (scale - mod(iTime, scale))/scale;
-vec2 sun_center = vec2(0.5, (0.2 + 0.6 * usr_p)) * iResolution.xy;
+vec2 sun_center = vec2(0.5, (0.2 + 0.3 * usr_p)) * iResolution.xy;
 vec2 pt = src_coord0 * iResolution.xy;
 
-float radius = 100.0 + usr_p * 300.0;
+float radius = 100.0 + usr_p * 100.0;
 if (distance(pt, sun_center) < radius) {
     color = vec4(1.0, 1.0, 1.0, 1.0);
 } else {
     color = vec4(0.0, 0.0, 0.0, 1.0);
 }
 
-if (sun_center.y >= pt.y && (sin(2 * fract(src_coord0.y + mod(frame,720.0)/720.0) * ((400.0 - radius)/radius * 5 + 5) * M_PI * 2.0) + 1) / 2.0 * (pt.y/(sun_center.y - 0.2 * iResolution.y)) > 0.8) {
+float sun_bottom = sun_center.y + radius;
+float dy = sun_bottom - pt.y;
+if (dy > 30.0 && 
+   (50 * pow(M_E, -8*((sun_bottom - dy)-90)/sun_bottom) * (1 + sin(((sun_bottom-dy)/sun_bottom + mod(frame,720.0)/720.0) * 2 * M_PI * 20))/2.0 >= 0.85)) {
     color = vec4(0.0, 0.0, 0.0, 1.0);
 }
 
