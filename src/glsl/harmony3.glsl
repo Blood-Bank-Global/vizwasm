5ce1e340899dd515c0f9f0ecba3848563e9e8745
vampireexec <131703298+vampireexec@users.noreply.github.com>
2025-07-13 17:10:45 -0400

    harmony 3 demo

--- src/glsl/harmony3.glsl
+++ src/glsl/harmony3.glsl
@@ -1,1 +1,28 @@
-color = vec4(1.0, 1.0, 0.0, 1.0);
+//!VAR int usr_var 0
+color = vec4(1.0, 0.0, 0.0, 1.0);
+
+float dim = 25.0;
+vec2 coord = src_coord0 * iResolution.xy;
+float row = floor(coord.y / dim);
+
+float vari = 3 + 3 * sin(mod(float(frame), 720.0)/720.0 * 2.0 * M_PI) * dim;
+
+if (mod(row, 2.0) == 1.0) {
+    coord.x +=  vari; // Offset for odd rows
+ } else {
+    coord.x -= vari; // Offset for even rows
+ }
+
+float col = floor(coord.x/ dim);
+
+if (mod(row, 2.0) == 0.0) {
+    col += 1; // Offset for even rows
+}
+
+if (mod(col, 2.0) == 0.0) {
+    color = vec4(0.0, 0.0, 0.0, 1.0); // Black
+}
+
+if (fract(coord.x/dim) < 0.05 || fract(coord.y/dim) < 0.05) {
+    color = vec4(0.2, 0.2, 0.2, 1.0);
+}
