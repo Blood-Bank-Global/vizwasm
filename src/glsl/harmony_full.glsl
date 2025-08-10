aae0e84a2e853d7a2b2e6bfce728f173eeaab9ba
vampireexec <131703298+vampireexec@users.noreply.github.com>
2025-08-10 21:52:51 +0100

    harmony full error pops

--- src/glsl/harmony_full.glsl
+++ src/glsl/harmony_full.glsl
@@ -1,6 +1,6 @@
+//!VAR int usr_var 0
 
 /**
-/* 
  * Notes:
  * 0. blank
  * 1. error2
@@ -13,7 +13,6 @@
  * 8. full
  */
 
-//!VAR usr_var 0
 
 vec4 below = texture(src_tex3, src_coord3);
 vec4 above = texture(src_tex4, src_coord4);
@@ -24,7 +23,27 @@ if (distance(above.rgb, vec3(0.0)) < 0.3) {
 
 color = blend_by_mode(below, above, BLEND_ALPHA);
 
-if (usr_var == 1) {
-    vec4 blast = texture(src_tex1, src_tex1);
-    color = blend_by_mode(color, blast, BLEND_SCREEN);
+switch (usr_var) {
+    case 0:
+        break;
+    case 1:
+        {
+            vec4 blast = texture(src_tex1, src_coord1);
+            if (distance(blast.rgb, vec3(0.0)) < 0.3) {
+                blast.a = 0.0;  // If blast is almost black, set alpha to 0
+            } 
+            color = blend_by_mode(color, blast, BLEND_ALPHA);
+            break;
+        }
+    case 2:
+        {
+            vec4 blast = texture(src_tex2, src_coord2);
+            if (distance(blast.rgb, vec3(0.0)) < 0.3) {
+                blast.a = 0.0;  // If blast is almost black, set alpha to 0
+            } 
+            color = blend_by_mode(color, blast, BLEND_ALPHA);
+            break;
+        }
+    default:
+        break;
 }
