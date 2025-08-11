988c9e4e611b301f4f3f4572be4c0cde35d1cf63
vampireexec <131703298+vampireexec@users.noreply.github.com>
2025-08-11 07:20:13 +0100

    add jitter

--- src/glsl/harmony1.glsl
+++ src/glsl/harmony1.glsl
@@ -1,8 +1,17 @@
 float thickness = 0.05;
 
-float delta = thickness * sin((mod(frame, 100.0) / 100.0 + src_coord0.x * 1.0) * 2.0 * M_PI) + 0.06;
+float wave1 = src_coord0.x * 4.0 * M_PI + fract(frame/30.0) * 2.0 * M_PI;
+float delta = thickness * cos(wave1) + thickness * 3.1;
 
-if (src_coord0.y >= (1.0 - delta)) {
+float wave2 =  fract(frame/200.0) * 2.0 * M_PI;
+float variation = thickness * cos(wave2);
+
+float wave3 = src_coord0.x * 12.0 * M_PI + fract(frame/30.0) * 2.0 * M_PI;
+float jitter = variation * cos(wave3);
+
+float superposition = delta + jitter * 0.0;
+
+if (src_coord0.y >= (1.0 - superposition)) {
   color = vec4(1.0, 1.0, 1.0, 1.0);  // White
 } else {
   color = vec4(0.0, 0.0, 0.0, 1.0);
