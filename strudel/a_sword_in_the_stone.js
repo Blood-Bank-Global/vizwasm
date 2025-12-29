6211b28ef7f9265af9974663d38dd99416a10155
vampireexec <131703298+vampireexec@users.noreply.github.com>
2025-12-29 17:43:22 -0500

    updates to add knob controls

--- strudel/a_sword_in_the_stone.js
+++ strudel/a_sword_in_the_stone.js
@@ -1,28 +1,30 @@
 setCpm(135 / 4);
 
+let akai = await midin('MPK mini 3');
+
 /////////////////////// BASS DRUM
 let bd_beat = beat("0.1,8.1", 16);
-
+let bd_gain = akai(70) /* slider(0.4665, 0, 1.5)*/;
 $: "dungeon_perc:3"
   .s()
   .apply(bd_beat)
-  .gain(slider(0.4665, 0, 1.5))
+  .gain(bd_gain)
   ._punchcard();
 
 $: "0"
   .apply(bd_beat)
-  .add.squeeze("1 0")
+  .when(bd_gain.gt(.1), x => x.add.squeeze("1 0"))
   .ccv()
   .ccn("0")
   .midi('IAC Driver Bus 1');
 
-
 /////////////////////////// TOMS
 let tom_beat = beat("0,2,4,6,8,10,12,14", 16);
+let tom_degrade = "1".sub(akai(71));
 let tom_beat2 = x => {
   return x
     .beat("0,2,4,6,8,10,12,14", 16)
-    .degradeBy(slider(0.496, 0, 1))
+    .degradeBy(tom_degrade)
     .rib(40, 2);
 };
 
@@ -31,7 +33,7 @@ $: tom_beat2(
     .n(irand(3)
       .add(7)
       .seg(4)))
-  .gain(.9)
+  .gain(1.5)
   ._punchcard();
 
 $: tom_beat2("0!4")
@@ -40,9 +42,16 @@ $: tom_beat2("0!4")
   .ccn("2")
   .midi('IAC Driver Bus 1');
 
+$: "0"
+  .when(tom_degrade.lt(.9), x => x.mask())
+  .ccv()
+  .ccn("2")
+  .midi('IAC Driver Bus 1');
+
 
 ///////////////////////// STRINGS
-let str_gain = 1.0;
+let str_gain = akai(72);
+
 $: s("dungeon_strings:0")
   .scrub(".7")
   .note("<b2 a2 e2 f2 b2 a2 e2 g2>"
@@ -50,13 +59,20 @@ $: s("dungeon_strings:0")
   .sus(.4)
   .gain(str_gain);
 
-$: stepcat([str_gain / 1.5, isaw.seg(8)], [1 - (str_gain / 1.5), "0!4"])
+$: stepcat([15 / 16, isaw.seg(8)], [1 / 16, "0!4"])
+  .when(str_gain.lt(.1), x => x.mask())
+  .ccv()
+  .ccn("1")
+  .midi('IAC Driver Bus 1');
+
+$: "0"
+  .when(str_gain.gte(.1), x => x.mask())
   .ccv()
   .ccn("1")
   .midi('IAC Driver Bus 1');
 
 ////////////////////////// PLUCKS
-let pluck_degrade = ".5";
+let pluck_degrade = "1".sub(akai(73));
 let pluck_beat = x => {
   return x.
     degradeBy(pluck_degrade)
@@ -87,7 +103,7 @@ $: "0"
   .midi('IAC Driver Bus 1');
 
 ///////////////////// SHAKER
-let shaker_degrade = ".5";
+let shaker_degrade = "1".sub(akai(74));
 let shaker_beat = x => {
   return x
     .degradeBy(shaker_degrade)
@@ -119,6 +135,7 @@ $: "0".when(shaker_degrade.lt(.95), x => x.mask())
   .midi('IAC Driver Bus 1');
 
 //////////////////////////////// BELLS
+let bell_gain = akai(75);
 let bell_beat = x => {
   return x
     .beat("0,8?", 16)
@@ -127,11 +144,18 @@ let bell_beat = x => {
 
 $: bell_beat(
   s("sleighbells:0")
-    .gain(slider(1.692, 0, 4))
+    .gain(bell_gain.mul(4))
 )
   ._punchcard();
 $: bell_beat("0")
+  .when(bell_gain.lt(.1), x => x.mask())
   .add.squeeze(stepcat([15 / 16, isaw.seg(4)], [1 / 16, 0]))
+  .ccv()
+  .ccn("7")
+  .midi('IAC Driver Bus 1');
+
+$: "0"
+  .when(bell_gain.gte(.1), x => x.mask())
   .ccv()
   .ccn("7")
   .midi('IAC Driver Bus 1');
