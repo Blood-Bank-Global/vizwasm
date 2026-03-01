5f2cf506a183a509eabb35b95468747bd4665b55
vampireexec <131703298+vampireexec@users.noreply.github.com>
2026-03-01 15:32:06 -0500

    add midi fighter support

--- strudel/a_cursed_mirror.js
+++ strudel/a_cursed_mirror.js
@@ -1,27 +1,79 @@
-setCpm(72 / 4);
-const picks = (x) => {
-    switch (x) {
-        case 'choir':
-            return 1;
-        case 'choir2':
-            return 1;
-        case 'tubular':
-            return 1;
-        case 'jingle':
-            return 1;
-        case 'pad':
-            return 1;
-        case 'chord':
-            return 1;
-        case 'filt':
-            return 1;
-        case 'metro':
-            return 0;
-        default:
-            return 0;
+if (!window.doNotUpdate) {
+    window.doNotUpdate = true;
+    window.myrepl = {}
+    window.myrepl.mapping = {
+        36: 'filt0',
+        37: 'filt1',
+        38: 'filt2',
+        39: 'metro',
+        //skip 40,41,42,43
+        44: 'pad',
+        45: 'chord',
+        46: 'drum',
+        //skip 47
+        48: 'choir',
+        49: 'choir2',
+        50: 'tubular',
+        51: 'jingle',
+    };
+    window.myrepl.values = {};
+    for (let cc in window.myrepl.mapping) {
+        window.myrepl.values[window.myrepl.mapping[cc]] = 0;
     }
+    window.myrepl.values['filt1'] = 1;
+
+    function onEnabled() {
+        let spectra_out = WebMidi.getOutputByName('Midi Fighter Spectra');
+        let spectra = WebMidi.getInputByName('Midi Fighter Spectra');
+        spectra.channels[4].removeListener()
+        spectra.channels[4].addListener('midimessage', e => {
+            const data = e.data;
+            if (data[0] != 179 || data[2] != 127) {
+                return;
+            }
+            let mapping = window.myrepl.mapping[data[1]];
+            if (mapping != undefined) {
+
+                if (mapping == 'filt0' || mapping == 'filt1' || mapping == 'filt2') {
+                    window.myrepl.values['filt0'] = window.myrepl.values['filt1'] = window.myrepl.values['filt2'] = 0;
+                    window.myrepl.values[mapping] = 1;
+                } else {
+                    window.myrepl.values[mapping] = window.myrepl.values[mapping] > 0 ? 0 : 1;
+                }
+                for (let mapping in window.myrepl.mapping) {
+                    if (window.myrepl.values[window.myrepl.mapping[mapping]] >= 1.0) {
+                        spectra_out.channels[3].sendNoteOn(mapping, 127)
+                    } else {
+                        spectra_out.channels[3].sendNoteOff(mapping, 0)
+                    }
+                }
+            }
+        });
+
+        for (let mapping in window.myrepl.mapping) {
+            if (window.myrepl.values[window.myrepl.mapping[mapping]] >= 1.0) {
+                spectra_out.channels[3].sendNoteOn(mapping, 127)
+            } else {
+                spectra_out.channels[3].sendNoteOff(mapping, 0)
+            }
+        }
+    };
+
+    WebMidi
+        .enable()
+        .then(onEnabled)
+        .catch(err => alert(err));
+}
+
+const picks = (x) => {
+    let v = window.myrepl.values[x];
+    if (v == undefined) {
+        return 0;
+    }
+    return ref(() => window.myrepl.values[x]);
 };
 
+setCpm(72 / 4);
 
 const choir_p = ["-", "d4"];
 $CHANT: s("gm_choir_aahs:7")
@@ -32,10 +84,10 @@ $CHANT: s("gm_choir_aahs:7")
     .rel(1.0)
     .room(.4)
     .delay(.2)
-    .postgain(.8)
+    .postgain(1.0)
     ._scope();
 
-const choir2_p = ["-", "c2"];
+const choir2_p = ["-", "d2"];
 $CHANT2:
 s("gm_fx_goblins:7")
     .note(pick(choir2_p, picks('choir2')))
@@ -44,7 +96,7 @@ s("gm_fx_goblins:7")
     .rel(1.0)
     .room(.4)
     .delay(.2)
-    .postgain(.8)
+    .postgain(2.0)
     ._scope();
 
 const pad_p = ["-", "<c4 d4 e4 c#4 d4 e4 f4 d4 e#4 g4 d4 e4>"]
@@ -65,6 +117,8 @@ $JINGLE: s("sleighbells:3")
     .delay(.2)
     .postgain(2.5)
     ._punchcard()
+const drum_p = ["-", "<[1 1] [1 1] [1 1] [1 1 1 1]>"];
+$DRUM: s("gm_taiko_drum:2").struct(pick(drum_p, picks('drum'))).hpf(75).postgain(2);
 
 const chord_p = [
     "-",
@@ -99,4 +153,4 @@ const filt_p = [
     "1.0"
 ];
 
-$FILT: djf(pick(filt_p, picks('filt'))).o("<0,1,2>").gain(0)
+$FILT: djf(pick(filt_p, picks('filt1').add(picks('filt2').mul(2)))).o("<0,1,2>").gain(0)
