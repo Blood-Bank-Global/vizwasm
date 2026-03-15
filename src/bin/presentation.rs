95e29ae9c7964d5e593d7a608edf735731d1d2c1
vampireexec <131703298+vampireexec@users.noreply.github.com>
2026-03-15 19:48:02 -0400

    start the presentation

---
+++ src/bin/presentation.rs
@@ -0,0 +1,333 @@
+use std::{
+    collections::HashMap,
+    error::Error,
+    sync::{LazyLock, Mutex},
+};
+
+#[allow(unused_imports)]
+use sdlrig::gfxinfo::{MIDI_CONTROL_CHANGE, MIDI_NOTE_OFF, MIDI_NOTE_ON};
+
+use sdlrig::{
+    gfxinfo::{Asset, GfxEvent, GfxInfo, MidiEvent, Vid, VidMixer},
+    renderspec::{Mix, RenderSpec},
+};
+
+use vizwasm::{beat_time_boilerplate, streamsettings::StreamSettingsField};
+use vizwasm::{
+    shaderhelper::include_files,
+    vizconfig::{time_code_2_float, AllSettings, MixConfig},
+};
+fn main() {}
+
+static STREAM_PATH: &'static str = "/Users/ttie/Desktop/presentation/streams";
+static TECH_PATH: &'static str = "/Users/ttie/Desktop/tech_streams";
+static ASSET_PATH: &'static str = "/Users/ttie/Desktop/common_data";
+
+static STREAM_DEFS: LazyLock<Vec<Vid>> = LazyLock::new(|| {
+    let mut vids = vec![];
+
+    let vid640x480: &[&str] = &[];
+    for vid_name in vid640x480.iter() {
+        vids.push(
+            Vid::builder()
+                .name(vid_name)
+                .path(format!("{STREAM_PATH}/{}.mp4", vid_name))
+                .resolution((640, 480))
+                .tbq((1, 12800))
+                .pix_fmt("yuv420p")
+                .repeat(true)
+                .realtime(false)
+                .hardware_decode(true)
+                .build(),
+        );
+    }
+
+    let tech_vids640x480: &[&str] = &["blank"];
+    for vid_name in tech_vids640x480.iter() {
+        vids.push(
+            Vid::builder()
+                .name(vid_name)
+                .path(format!("{TECH_PATH}/{}.mp4", vid_name))
+                .resolution((640, 480))
+                .tbq((1, 12800))
+                .pix_fmt("yuv420p")
+                .repeat(true)
+                .realtime(false)
+                .hardware_decode(true)
+                .build(),
+        );
+    }
+
+    let pngs640x480: &[&str] = &[];
+    for png_name in pngs640x480.iter() {
+        vids.push(
+            Vid::builder()
+                .name(png_name)
+                .path(format!("{STREAM_PATH}/{}.png", png_name))
+                .resolution((640, 480))
+                .tbq((1, 12800))
+                .pix_fmt("yuv420p")
+                .repeat(true)
+                .realtime(false)
+                .hardware_decode(true)
+                .build(),
+        );
+    }
+
+    //Cameras
+    vids.push(
+        Vid::builder()
+            .name("front cam")
+            .path("MacBook Pro Camera")
+            // .path("Logitech BRIO")
+            .format("avfoundation")
+            .opts(&vec![
+                ("pixel_format", "bgr0"),
+                ("framerate", "30.0"),
+                ("video_size", "1280x720"),
+                // ("video_size", "640x360"),
+                // ("video_size", "1920x1080"),
+                ("fflags", "+nobuffer+flush_packets"),
+                ("probesize", "32"),
+                ("flags", "low_delay"),
+                ("analyzeduration", "0"),
+                ("rtbufsize", "5000000"),
+            ])
+            .resolution((1280, 720))
+            // .resolution((640, 360))
+            // .resolution((1920, 1080))
+            .tbq((1, 1000000))
+            .pix_fmt("bgr0")
+            .repeat(false)
+            .realtime(true)
+            .hardware_decode(false)
+            .build()
+            .into(),
+    );
+    vids
+});
+
+static PLAYBACK_NAMES: LazyLock<Vec<String>> = LazyLock::new(|| {
+    let names = ["blank", "demo_fonts", "front cam"]
+        .iter()
+        .map(|s| s.to_string())
+        .collect::<Vec<_>>();
+    names
+});
+
+static MIX_CONFIGS: LazyLock<Vec<MixConfig>> = LazyLock::new(|| {
+    let mut configs = vec![];
+
+    for vid in STREAM_DEFS.iter() {
+        let mix_name = format!("{}_mix", vid.name);
+        configs.push(MixConfig {
+            def: VidMixer::builder()
+                .name(mix_name.clone())
+                .width(vid.resolution.0 as u32)
+                .height(vid.resolution.1 as u32)
+                .build(),
+            mix: Mix::builder()
+                .name(mix_name.clone())
+                .video(&vid.name)
+                .no_display(true)
+                .build(),
+        });
+    }
+
+    configs.push(MixConfig {
+        def: VidMixer::builder()
+            .name("demo_fonts_mix")
+            .width(640)
+            .height(480)
+            .shader(include_files(include_str!("../glsl/demo_fonts.glsl")))
+            .build(),
+        mix: Mix::builder()
+            .name("demo_fonts_mix")
+            .mixed("blank_mix")
+            .no_display(true)
+            .build(),
+    });
+
+    configs
+});
+
+static SETTINGS: LazyLock<Mutex<Box<AllSettings>>> = LazyLock::new(|| {
+    let settings = AllSettings::new(
+        STREAM_DEFS.clone(),
+        MIX_CONFIGS.clone(),
+        PLAYBACK_NAMES.clone(),
+        ASSET_PATH,
+    );
+    Mutex::new(Box::new(settings))
+});
+
+#[no_mangle]
+#[allow(unused)]
+pub fn asset_list(fps: i64) -> Vec<Asset> {
+    let mut lock = if let Ok(lock) = SETTINGS.lock() {
+        lock
+    } else {
+        panic!("Settings object has been corrupted.");
+    };
+
+    let settings = lock.as_mut();
+
+    if PLAYBACK_NAMES
+        .iter()
+        .map(|s| s.to_string())
+        .collect::<Vec<_>>()
+        != settings.playback_names
+    {
+        let orig = settings.clone();
+        *settings = AllSettings::new(
+            STREAM_DEFS.clone(),
+            MIX_CONFIGS.clone(),
+            PLAYBACK_NAMES.clone(),
+            STREAM_PATH,
+        );
+        eprintln!("settings playback names: {:?}", settings.playback_names);
+        for i in 0..orig.playback.len() {
+            for j in 0..settings.playback.len() {
+                if settings.playback[j].stream.ident.name == orig.playback[i].stream.ident.name {
+                    // If the playback name matches, copy the stream.
+                    let ident = settings.playback[j].stream.ident.clone();
+                    settings.playback[j].stream = orig.playback[i].stream.clone();
+                    settings.playback[j].stream.ident = ident;
+                }
+            }
+        }
+    }
+
+    settings.asset_list(fps)
+}
+
+#[no_mangle]
+pub fn encode_settings() -> Vec<u8> {
+    let mut lock = SETTINGS.lock().expect("Could not get settings lock.");
+    let settings = lock.as_mut();
+    serde_json::to_vec(settings).unwrap()
+}
+
+#[no_mangle]
+pub fn decode_settings(bytes: &[u8]) {
+    let mut lock = SETTINGS.lock().expect("Could not get settings lock.");
+    let settings = lock.as_mut();
+    let decoded =
+        serde_json::from_slice::<AllSettings>(bytes).expect("Failed to decode settings from bytes");
+    settings.scan_idx = decoded.scan_idx;
+    settings.active_idx = decoded.active_idx;
+    settings.display_idx = decoded.display_idx;
+    for i in 0..decoded.playback.len() {
+        if i >= settings.playback.len() {
+            break;
+        }
+        if settings.playback[i].stream.ident == decoded.playback[i].stream.ident {
+            settings.playback[i] = decoded.playback[i].clone();
+        }
+    }
+}
+
+#[no_mangle]
+pub fn calculate(
+    #[allow(unused)] canvas_w: u32,
+    #[allow(unused)] canvas_h: u32,
+    #[allow(unused)] frame: i64,
+    #[allow(unused)] fps: i64,
+    #[allow(unused)] gfx_info: &HashMap<String, GfxInfo>,
+    #[allow(unused)] reg_events: &[GfxEvent],
+) -> Result<Vec<RenderSpec>, Box<dyn Error>> {
+    let mut lock = SETTINGS.lock().expect("Settings mutex corrupted");
+    let settings = lock.as_mut();
+    Ok(settings.update_record_and_get_specs(reg_events, frame, Some(mega_cb))?)
+}
+
+const IAC: &str = "IAC Driver Bus 1";
+pub fn mega_cb(all_settings: &mut AllSettings, event: &MidiEvent) {
+    a_sword_in_the_stone_cb(all_settings, event);
+    alien_cb(all_settings, event);
+    city_hunter_cb(all_settings, event);
+}
+
+pub fn a_sword_in_the_stone_cb(_all_settings: &mut AllSettings, event: &MidiEvent) {
+    let time_codes = [
+        "00:00:35:21",
+        "00:00:43:20",
+        "00:00:47:15",
+        "00:00:54:15",
+        "00:01:00:18",
+        "00:01:07:00",
+        "00:01:11:03",
+        "00:01:31:18",
+        "00:01:36:50",
+        "00:01:39:37",
+        "00:01:48:14",
+        "00:01:55:39",
+        "00:02:00:22",
+        "00:02:05:36",
+        "00:02:15:05",
+        "00:02:20:32",
+    ]
+    .iter()
+    .map(|s| time_code_2_float(s))
+    .collect::<Vec<_>>();
+
+    beat_time_boilerplate!(
+        _all_settings,
+        event,
+        "a_sword_in_the_stone",
+        "a_sword_in_the_stone_combo",
+        time_codes
+    );
+}
+
+pub fn alien_cb(all_settings: &mut AllSettings, event: &MidiEvent) {
+    let text_cam = all_settings
+        .playback
+        .iter_mut()
+        .find(|p| p.stream.ident.name == "text cam");
+    if let Some(pb) = text_cam {
+        match (event.kind, event.device.as_str(), event.key, event.velocity) {
+            (MIDI_CONTROL_CHANGE, IAC, 0, value) => {
+                if value > 10 {
+                    pb.stream.set_field(StreamSettingsField::WarpLevel, 0.05);
+                } else {
+                    pb.stream.set_field(StreamSettingsField::WarpLevel, 0.0);
+                }
+            }
+            _ => {}
+        }
+    }
+}
+
+pub fn city_hunter_cb(all_settings: &mut AllSettings, event: &MidiEvent) {
+    beat_time_boilerplate!(
+        all_settings,
+        event,
+        "CityHunter",
+        "CityHunter",
+        vec![
+            time_code_2_float("00:00:01:23"),
+            time_code_2_float("00:00:05:02"),
+            time_code_2_float("00:00:08:13"),
+            time_code_2_float("00:00:16:16"),
+            time_code_2_float("00:00:22:01"),
+            time_code_2_float("00:00:23:19"),
+            time_code_2_float("00:00:35:17"),
+            time_code_2_float("00:00:54:21"),
+            time_code_2_float("00:00:56:14"),
+            time_code_2_float("00:01:05:12"),
+            time_code_2_float("00:01:10:06"),
+            time_code_2_float("00:01:13:15"),
+            time_code_2_float("00:01:16:22"),
+            time_code_2_float("00:01:18:13"),
+            time_code_2_float("00:01:21:04"),
+            time_code_2_float("00:01:23:04"),
+            time_code_2_float("00:01:25:07"),
+            time_code_2_float("00:01:27:06"),
+            time_code_2_float("00:01:43:15"),
+            time_code_2_float("00:01:49:12"),
+            time_code_2_float("00:01:55:03"),
+            time_code_2_float("00:01:59:12"),
+        ]
+    );
+}
