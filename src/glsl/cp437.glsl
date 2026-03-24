176f9345e009a7d654fc3ff30ea30466a572b884
vampireexec <131703298+vampireexec@users.noreply.github.com>
2026-03-23 22:45:21 -0400

    add a cp437 sheet

---
+++ src/glsl/cp437.glsl
@@ -0,0 +1,40 @@
+#include "utils.glsl"
+#include "font_8x16.glsl"
+#define FONT_W (font_8x16_width)
+#define FONT_H (font_8x16_height)
+
+void pass0(out vec4 color) {
+    vec2 uv = src_coord.xy * iResolution.xy;
+    vec2 rc = floor(uv / vec2(FONT_W, FONT_H));
+    float idx = (rc.x - 1) + (rc.y - 1) * 16.0;
+    vec2 pos = rc * vec2(FONT_W, FONT_H);
+
+    color = vec4(0.0, 0.0, 0.0, 1.0);
+    if (rc.x >= 17.0 || rc.y >= 17.0) {
+        return;
+    }
+
+    vec4 letter_color = vec4(1.0);
+    uint c[1] = uint[1](uint(idx));
+    if (uint(rc.x) == 0) {
+        letter_color = vec4(0.0, 0.0, 0.0, 1.0);
+        color = vec4(1.0);
+         if (uint(rc.y) >= 10u) {
+            c[0] = uint(rc.y) - 11 + 0x41;
+        } else {
+            c[0] = uint(rc.y) - 1 + 0x30;
+        }
+    } else if (uint(rc.y) == 0) {
+        letter_color = vec4(0.0, 0.0, 0.0, 1.0);
+        color = vec4(1.0);
+         if (uint(rc.x) >= 10u) {
+            c[0] = uint(rc.x) - 11 + 0x41;
+        } else {
+            c[0] = uint(rc.x) - 1 + 0x30;
+        }
+    }
+    
+    if (font_8x16(uv, pos, c, 0, 1)) {
+        color = letter_color;
+    }
+}
